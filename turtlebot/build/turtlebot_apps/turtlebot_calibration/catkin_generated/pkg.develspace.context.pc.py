566df4bdf07d1dee474a10d4d823c22060a4daf5
dawidjozefczak <dj426@wp.pl>
2018-06-14 12:35:15 +0200

    Remove unnecessary files

--- turtlebot/build/turtlebot_apps/turtlebot_calibration/catkin_generated/pkg.develspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "/home/wojciech/turtlebot/devel/include".split(';') if "/home/wojciech/turtlebot/devel/include" != "" else []
-PROJECT_CATKIN_DEPENDS = "std_msgs;message_runtime".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "".split(';') if "" != "" else []
-PROJECT_NAME = "turtlebot_calibration"
-PROJECT_SPACE_DIR = "/home/wojciech/turtlebot/devel"
-PROJECT_VERSION = "2.3.7"
--- turtlebot/build/turtlebot_create/create_node/catkin_generated/safe_execute_install.cmake
+++
@@ -1,5 +0,0 @@
-execute_process(COMMAND "/home/wojciech/turtlebot/build/turtlebot_create/create_node/catkin_generated/python_distutils_install.sh" RESULT_VARIABLE res)
-
-if(NOT res EQUAL 0)
-  message(FATAL_ERROR "execute_process(/home/wojciech/turtlebot/build/turtlebot_create/create_node/catkin_generated/python_distutils_install.sh) returned error code ")
-endif()
